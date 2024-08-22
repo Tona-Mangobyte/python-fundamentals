3e6fd8e0ec2f5867e44fcb567835d77a65ca5248
Tona <tona@mango-byte.com>
2024-08-22 21:56:30 +0700

    add

---
+++ src/quotes.py
@@ -0,0 +1,10 @@
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow!",
+    "Curly": "Nyuk nyuk!",
+}
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
+
+language = 9
+print(f"Language {language}: I am Python. What's for supper?")
